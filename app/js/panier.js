3860634079157b3e8bd3a6ede7fb04a529e7b92c
Godbille Pascal <pascal.godbille@orange.fr>
2020-10-31 18:05:45 +0100

    panier remove

--- app/js/panier.js
+++ app/js/panier.js
@@ -2,7 +2,7 @@ let keyPanier = []
 let panier = {}
 panier.products = []
 let prixPanier =[]
-
+let quantiteOurs = []
 //////////////////recuperation panier//////////////////////////
 
 for(let i = 0; i < localStorage.length; i++) {
@@ -20,6 +20,7 @@ for (let i = 0; i < panier.products.length;i++) {
         prixPanier[i] = `${panier.products[i].price / 100}`
         let target = document.querySelector(".panier");
         let importPanier = document.createElement("tr");
+        importPanier.classList.add("produit")
         target.appendChild(importPanier);
         importPanier.innerHTML = ` <td data-th="Produit">
                                         <div class="row">
@@ -33,38 +34,61 @@ for (let i = 0; i < panier.products.length;i++) {
                                             </div>
                                         </div>
                                     </td>
-                                    <td class ="prixOurs"data-th="Price">${panier.products[i].price / 100}€</td>
+                                    <td class ="prixOurs" data-th="Price">${panier.products[i].price / 100}€</td>
                                     <td data-th="Quantity">
                                     
-                                        <input type="number" class="qteOurs" >
+                                        <input type="number" class="nbOurs form-control form-control-lg text-center" value="1">
                                     </td>
                                     <td class="actions" data-th="">
                                         <div class="text-right">
                                             <button class="recalculer btn btn-white border-secondary bg-white btn-md mb-2">
-                                                <i class="fas fa-sync">recalculer</i>
+                                                <i class="fas fa-sync"></i>
                                             </button>
-                                            <button class=" btn btn-white border-secondary bg-white btn-md mb-2">
+                                            <button class="reset btn btn-white border-secondary bg-white btn-md mb-2">
                                                 <i class="fas fa-trash"></i>
                                             </button>
-                                            <td  class ="prixTotalOurs" data-th="Sous total"></td>
+                                            <td  class ="prixTotalOurs text-center" data-th="Sous total">€</td>
                                             <div></div>
                                         </div>
                                     </td>`;
-        
-        
-        
-            
-     
-
-
 
         
     }
-}      
+ }
+   
+let nbOurs = document.querySelectorAll(".nbOurs")
+let recal = document.querySelectorAll(".recalculer")
+let subTotal = document.querySelectorAll(".prixTotalOurs")
+let reset = document.querySelectorAll(".reset")
+let divpanier = document.querySelector(".panier")
+let produit = document.querySelectorAll(".produit")
+
+
+
+for (let value of recal) {
+         for (let i = 0; i < nbOurs.length; i++) {
+          subTotal[i].innerHTML = nbOurs[i].value * prixPanier[i] + "€"
+        }
     
+}
 
+ 
 
-
+for (let value of recal) {
+    value.addEventListener("click", function () {
+        for (let i = 0; i < nbOurs.length; i++) {
+            
+          subTotal[i].innerHTML = nbOurs[i].value * prixPanier[i] + "€"
+        }
+    })
+}   
+    
+for (let i = 0; i < reset.length; i++) {
+    reset[i].addEventListener("click", function () {
+        divpanier.removeChild(produit[i])
+        localStorage.removeItem()
+    })
+}   
 
 
 
