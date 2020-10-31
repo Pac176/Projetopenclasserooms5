213b8b40c83301c95f1de71eeb93359cb75f1324
Godbille Pascal <pascal.godbille@orange.fr>
2020-10-31 11:00:12 +0100

    panier

--- app/js/panier.js
+++ app/js/panier.js
@@ -1,50 +1,39 @@
+let keyPanier = []
 let panier = {}
-panier.products =[]
+panier.products = []
+let prixPanier =[]
 
+//////////////////recuperation panier//////////////////////////
 
-
-let produits = ["Arnold & Dark brown",
-"Arnold & Pale brown", 
-"Arnold & White",
-"Garfunkel & Beige",
-"Garfunkel & Chocolate",
-"Garfunkel & Tan",
-"Gustav & Blue",
-"Gustav & Brown",
-"Gustav & Pink",
-"Lenny and Carl & Brown",
-"Norbert & Chocolate",
-"Norbert & Tan",
-"Norbert & White",
-"Norbert & Black"]
-
-for (let value of produits) {
-   
-    panier.products.push(JSON.parse(localStorage.getItem(value)))
-    
- 
+for(let i = 0; i < localStorage.length; i++) {
+     keyPanier[i] = localStorage.key(i)
+ panier.products.push(JSON.parse(localStorage.getItem(keyPanier[i])))
 }
 
-for (let value of panier.products) {
-    if (value) {
-        
+ 
+///////////////////injection dom produits achetés//////////////////////
+
+
+
+for (let i = 0; i < panier.products.length;i++) {
+    if (panier.products[i]) {
+        prixPanier[i] = `${panier.products[i].price / 100}`
         let target = document.querySelector(".panier");
         let importPanier = document.createElement("tr");
         target.appendChild(importPanier);
-        
         importPanier.innerHTML = ` <td data-th="Produit">
                                         <div class="row">
                                             <div class="col-md-3 text-left">
-                                                <img src=${value.imageUrl} alt=""
+                                                <img src=${panier.products[i].imageUrl} alt=""
                                                     class="img-fluid d-none d-md-block rounded mb-2 shadow ">
                                             </div>
                                             <div class="col-md-9 text-left mt-sm-2">
-                                                <h4>${value.name}</h4>
-                                                <p class="font-weight-light"><em>Couleur: ${value.colors}</em></p>
+                                                <h4>${panier.products[i].name}</h4>
+                                                <p class="font-weight-light"><em>Couleur: ${panier.products[i].colors}</em></p>
                                             </div>
                                         </div>
                                     </td>
-                                    <td data-th="Price">${value.price / 100}€</td>
+                                    <td class ="prixOurs"data-th="Price">${panier.products[i].price / 100}€</td>
                                     <td data-th="Quantity">
                                     
                                         <input type="number" class="qteOurs" >
@@ -57,12 +46,14 @@ for (let value of panier.products) {
                                             <button class=" btn btn-white border-secondary bg-white btn-md mb-2">
                                                 <i class="fas fa-trash"></i>
                                             </button>
-                                            <td class = "prixTotalOurs" data-th="Prix total"></td>
+                                            <td  class ="prixTotalOurs" data-th="Sous total"></td>
                                             <div></div>
                                         </div>
                                     </td>`;
         
         
+        
+            
      
 
 
@@ -71,16 +62,6 @@ for (let value of panier.products) {
     }
 }      
     
-
-
-
-
-
-
-   
-          
-
-             
 
 
 
