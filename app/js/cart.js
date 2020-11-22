b38ee6f820db44450949d70cfa8f0013528426f1
Godbille Pascal <pascal.godbille@orange.fr>
2020-11-22 11:56:41 +0100

    Simplification total cart

--- app/js/cart.js
+++ app/js/cart.js
@@ -2,108 +2,135 @@
 /////////////////////////class panier//////////////////////////
 
 export class Cart {
-  constructor() {
-    this.products = []
-    this.products = JSON.parse(localStorage.getItem("cart"))
-  }
+    constructor() {
+        this.products = []
+        this.products = JSON.parse(localStorage.getItem("cart"))
+    }
 
-///////////////////////////lien vers validation de commande  
-   validCommand() {
-       document.location.href = "http://127.0.0.1:5500/app/pages/validation.html"
-    } 
-//////////////////////////pastille notification
+    ///////////////////////////lien vers validation de commande  
+    validCommand() {
+        document.location.href = "http://127.0.0.1:5500/app/pages/validation.html"
+    }
+    //////////////////////////pastille notification
     get cartNotification() {
         this.products = JSON.parse(localStorage.getItem("cart"))
         /////////////////////////////si produit dans localstorage
         if (JSON.parse(localStorage.getItem("cart"))) {
-                ////////////////////et si n'existe pas alors création
-                if (document.querySelector(".pastillePanier") == null) {
-                    let logoPanier = document.querySelector(".logoPanier")
-                    let pastille = document.createElement("notification")
-                    pastille.classList.add("pastillePanier")
-                    logoPanier.insertAdjacentElement("afterbegin",pastille)
-                    pastille.innerHTML = this.products.length
-                } else {
+            ////////////////////et si n'existe pas alors création
+            if (document.querySelector(".pastillePanier") == null) {
+                let logoPanier = document.querySelector(".logoPanier")
+                let pastille = document.createElement("notification")
+                pastille.classList.add("pastillePanier")
+                logoPanier.insertAdjacentElement("afterbegin", pastille)
+                pastille.innerHTML = this.products.length
+            } else {
                 /////////////////////sinon juste mise a jour
-                    let pastille = document.querySelector(".pastillePanier")
-                    pastille.innerHTML = this.products.length
-                    pastille.classList.add("pastillePanier")
+                let pastille = document.querySelector(".pastillePanier")
+                pastille.innerHTML = this.products.length
+                pastille.classList.add("pastillePanier")
             }
-        } 
+        }
     }
-/////////////////////////envoi sur localstorage de Quantité pour utilisation dans la page formulaire  
+    /////////////////////////envoi sur localstorage de Quantité pour utilisation dans la page formulaire  
     get qteOursInLocalStorage() {
         let qte = []
         let nbOurs = document.querySelectorAll(".nbOurs");
-            for (let i = 0; i < nbOurs.length;i++){
-                qte[i] = parseInt(nbOurs[i].value)
-                    const reducer = (accumulator, currentValue) => parseInt(accumulator) + parseInt(currentValue);
-                    localStorage.setItem("Quantité", qte.reduce(reducer)) 
+        for (let i = 0; i < nbOurs.length; i++) {
+            qte[i] = parseInt(nbOurs[i].value)
+            const reducer = (accumulator, currentValue) => parseInt(accumulator) + parseInt(currentValue);
+            localStorage.setItem("Quantité", qte.reduce(reducer))
         }
     }
-////////////////////////effacer item du panier
+    ////////////////////////effacer item du panier
     deleteFromCart() {
         let divpanier = document.querySelector(".products")
         let reset = document.querySelectorAll(".reset");
         let produit = document.querySelectorAll(".produit");
         /////////////////////////////boucle sur les boutton supprimer
-            for (let i = 0; i < reset.length; i++) {
-                reset[i].addEventListener("click", () => {
-                    this.products = JSON.parse(localStorage.getItem("cart"));
-                    this.products.splice(reset[i], 1)
-                    /////////////////si dernier produit et confirmation alors panier vide
-                    if (this.products.length == 0) { 
-                        if (confirm("Etes vous sur de vouloir supprimer ce produit?")){
-                            divpanier.removeChild(produit[i]);     
-                            localStorage.clear();
-                            let logoPanier = document.querySelector(".logoPanier")
-                            let pastille = document.querySelector(".pastillePanier")
-                            logoPanier.removeChild(pastille)  
-                            let panier = document.querySelector(".panier")
-                            panier.innerHTML = `<div class="alert alert-warning" role="alert">
+        for (let i = 0; i < reset.length; i++) {
+            reset[i].addEventListener("click", () => {
+                this.products = JSON.parse(localStorage.getItem("cart"));
+                this.products.splice(reset[i], 1)
+                /////////////////si dernier produit et confirmation alors panier vide
+                if (this.products.length == 0) {
+                    if (confirm("Etes vous sur de vouloir supprimer ce produit?")) {
+                        divpanier.removeChild(produit[i]);
+                        localStorage.clear();
+                        let logoPanier = document.querySelector(".logoPanier")
+                        let pastille = document.querySelector(".pastillePanier")
+                        logoPanier.removeChild(pastille)
+                        let panier = document.querySelector(".panier")
+                        panier.innerHTML = `<div class="alert alert-warning" role="alert">
                                      Panier vide!
                                                                     </div>`
-                        } 
+                    }
                     ////////////////si pas dernier alors juste effacer l'item
-                    }else if (confirm("Etes vous sur de vouloir supprimer ce produit?")){
-                        localStorage.setItem("cart", JSON.stringify(this.products))
-                        divpanier.removeChild(produit[i]);
-                        this.prixTotal
-                        this.cartNotification
-                }      
+                } else if (confirm("Etes vous sur de vouloir supprimer ce produit?")) {
+                    localStorage.setItem("cart", JSON.stringify(this.products))
+                    divpanier.removeChild(produit[i]);
+                    this.prixTotal
+                    this.cartNotification
+                }
             })
         }
     }
+    get prixTotalInLocalstorage() {
+        let prixPanier = [] 
+        let prixTotal = document.querySelector(".prixTotal ");
+        let subTotal = document.querySelectorAll(".prixTotalOurs");
+        let nbOurs = document.querySelectorAll(".nbOurs");
+         let prixOurs = document.querySelectorAll(".prixOurs")
+        for (let i = 0; i < subTotal.length; i++) {
+            subTotal[i].innerHTML = nbOurs[i].value * parseInt(prixOurs[i].innerHTML) + "€";
+            prixPanier[i] = parseInt(subTotal[i].innerHTML)//////////////////recuperation des prix pour calcul total
+            const reducer = (accumulator, currentValue) => parseInt(accumulator) + parseInt(currentValue);
+            prixTotal.innerHTML = prixPanier.reduce(reducer) + "€";
+            localStorage.setItem("total", prixTotal.innerHTML) ////envoi sur le localstorage pour utilisation
+            this.qteOursInLocalStorage
+        }
+    }
+
 ///////////////////////Fonction calcul du total commande
     get prixTotal() {
         //////////si il y a des produits dans le panier avec par defaut quantité a 1
         if (this.products.length > 0) {
-            let prixPanier = []   /////prix des differents items du panier
-            for (let i = 0; i < this.products.length; i++) {
-                prixPanier[i] = `${this.products[i].price / 100}`//////////////////recuperation des prix pour calcul total
-            }
-            let prixTotal = document.querySelector(".prixTotal ");
-            let subTotal = document.querySelectorAll(".prixTotalOurs");
-            let prixOurs = document.querySelectorAll(".prixOurs")
             let nbOurs = document.querySelectorAll(".nbOurs");
-        
+            // let prixPanier = []   /////prix des differents items du panier
+            // for (let i = 0; i < this.products.length; i++) {
+            //     prixPanier[i] = `${this.products[i].price / 100}`//////////////////recuperation des prix pour calcul total
+            // }
+            
+           
+           
+            
+              /////prix des differents items du panier
             //let nbItems = document.querySelector(".nbItems")
             //qte[0] = nbItems.innerHTML
             ////////////////////////////////////// methode reduce sur la tableau de prix pour avoir la somme
-            const reducer = (accumulator, currentValue) => parseInt(accumulator) + parseInt(currentValue);
-            prixTotal.innerHTML = prixPanier.reduce(reducer) + "€";
-            localStorage.setItem("total", prixTotal.innerHTML) ////envoi sur le localstorage pour utilisation
+            // const reducer = (accumulator, currentValue) => parseInt(accumulator) + parseInt(currentValue);
+            // prixTotal.innerHTML = prixPanier.reduce(reducer) + "€";
+            // localStorage.setItem("total", prixTotal.innerHTML) ////envoi sur le localstorage pour utilisation
             //////////////////////////////////////calcul des sous totaux pour recalcul du total en fonction des quantités
-            for (let i = 0; i < subTotal.length; i++) {
-                subTotal[i].innerHTML = nbOurs[i].value * parseInt(prixOurs[i].innerHTML) + "€";
-                this.qteOursInLocalStorage
-                    nbOurs[i].addEventListener("change",  () => {
-                    subTotal[i].innerHTML = nbOurs[i].value * parseInt(prixOurs[i].innerHTML) + "€";
-                    prixPanier[i] = parseInt(subTotal[i].innerHTML);
-                    const reducer = (accumulator, currentValue) => parseInt(accumulator) + parseInt(currentValue);
-                    prixTotal.innerHTML = prixPanier.reduce(reducer) + "€";
-                    localStorage.setItem("total", prixTotal.innerHTML);
-                    this.qteOursInLocalStorage
+            for (let i = 0; i < nbOurs.length; i++) {
+
+                
+                this.prixTotalInLocalstorage
+                // subTotal[i].innerHTML = nbOurs[i].value * parseInt(prixOurs[i].innerHTML) + "€";
+                // prixPanier[i] =  parseInt(subTotal[i].innerHTML)//////////////////recuperation des prix pour calcul total
+                // const reducer = (accumulator, currentValue) => parseInt(accumulator) + parseInt(currentValue);
+                // prixTotal.innerHTML = prixPanier.reduce(reducer) + "€";
+                // localStorage.setItem("total", prixTotal.innerHTML) ////envoi sur le localstorage pour utilisation
+                // this.qteOursInLocalStorage
+
+
+                nbOurs[i].addEventListener("change", () => {
+                        this.prixTotalInLocalstorage
+                    // subTotal[i].innerHTML = nbOurs[i].value * parseInt(prixOurs[i].innerHTML) + "€";
+                    // prixPanier[i] = parseInt(subTotal[i].innerHTML);
+                    // const reducer = (accumulator, currentValue) => parseInt(accumulator) + parseInt(currentValue);
+                    // prixTotal.innerHTML = prixPanier.reduce(reducer) + "€";
+                    // localStorage.setItem("total", prixTotal.innerHTML);
+                    // this.qteOursInLocalStorage
                })
             }
         } 
